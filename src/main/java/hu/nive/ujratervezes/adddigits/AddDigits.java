6734193daad118977108aaf3ad177ec9315a0943
marcellkrausz <krauszmaarcell@gmail.com>
2021-04-29 10:16:24 +0200

    add solution

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,19 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public static int addDigits(String input) {
+        if(input == null || input.equals("")) {
+            return -1;
+        }
+        char[] inputArray = input.toCharArray();
+        int sum = 0;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (Character.isDigit(inputArray[i])) {
+                sum += Integer.parseInt(String.valueOf(inputArray[i]));
+            }
+        }
+        return sum;
+    }
 }
